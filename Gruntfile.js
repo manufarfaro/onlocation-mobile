a87c8f45de71ed80ab54e5283217d046fbf55dad
manufarfaro <manufarfaro@gmail.com>
2015-03-05 16:26:44 -0300

    Initial Files.

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    uglify: {
+      target: {
+        files: {
+          'public/js/app.min.js': [
+          'public/vendor/jquery/dist/jquery.js',
+          'public/vendor/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
+          'public/js/main.js'
+          ]
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+      {expand: true, flatten: true, src: ['public/vendor/font-awesome-sass/assets/fonts/font-awesome/*'], dest: 'public/fonts/font-awesome/', filter: 'isFile'},
+    {expand: true, flatten: true, src: ['public/vendor/bootstrap-sass-official/assets/fonts/bootstrap/*'], dest: 'public/fonts/', filter: 'isFile'}
+    ]
+  }
+},
+watch: {
+  files: ['<%= jshint.files %>', 'public/sass/*', 'public/js/main.js'],
+  tasks: ['jshint', 'uglify', 'sass']
+},
+sass: {
+  dist: {
+    files: {
+      'public/css/app.min.css': 'public/sass/main.scss'
+    }
+  }
+}
+});
+
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('build', ['jshint', 'sass', 'uglify', 'copy:main']);
+grunt.registerTask('default', ['build', 'watch']);
+
+};
