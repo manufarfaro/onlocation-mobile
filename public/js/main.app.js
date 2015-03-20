d4b66b015921203c5452deff58239282f1484048
blas <blas.moyano@gmail.com>
2015-03-19 23:12:10 -0300

    Avance diseño

--- public/js/main.app.js
+++ public/js/main.app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
   $('.multiple-items').slick({
     infinite: true,
@@ -8,4 +9,16 @@ $(document).ready(function(){
   $("#owl-demo").owlCarousel();
   $("#owl-demo2").owlCarousel();
 
-})
+});
+
+$(document).ready(function(){
+$("#owl-demo3").owlCarousel({
+
+  autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+    items : 3,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3]
+
+ });
+});
